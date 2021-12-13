7127d8e7c28e7a7d76375217c172efcb8cf1d015
Nur tusa <nur.tusa@student.moringaschool.com>
2021-12-13 19:18:58 +0300

    my first model

--- prize/models.py
+++ prize/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Project(models.Model):
+    img = models.ImageField(default='default.png', upload_to='images')
+    title = models.CharField(default='My Project', max_length = 40)
+    description = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    reviews = models.CharField(max_length = 40, blank = True, default = 0)
+    link = models.CharField(default='No link', max_length = 120)
+    av_usability = models.CharField(max_length = 40, default = 0)
+    av_design = models.CharField(max_length = 40, default = 0)
+    av_content = models.CharField(max_length = 40, default = 0)
+    rating = models.CharField(max_length = 40, default = 0)
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projectis = cls.objects.filter(title__icontains=search_term)
+        return projectis
+
+    def get_absolute_url(self):
+        return reverse('project-detail', kwargs={'pk': self.pk})
+
+
+    def get_project_by_id(project_id):
+        project = Project.objects.get(pk = project_id)
+        return project
--- prize/urls.py
+++ prize/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.conf.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
+  path('welcome', views.welcome, name = 'awwards-welcome'),
+
   path("",views.index,name="home")
 ]
